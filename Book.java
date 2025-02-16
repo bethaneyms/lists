3a61278ce8c03199fbca7517dc3636ac1e967299
bethaneyms <bethaneym03@gmail.com>
2025-02-15 20:33:16 -0600

    Create Book.java

---
+++ Book.java
@@ -0,0 +1,26 @@
+public class Book {
+    private String title;
+    private String author;
+    private int pages;
+
+    public Book(String title, String author, int pages) {
+        this.title = title;
+        this.author = author;
+        this.pages = pages;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+    public String getAuthor() {
+        return author;
+    }
+    public int getPages() {
+        return pages;
+    }
+
+    public String toString() {
+        return "Title: " + title + ", Author: " + author + ", Pages: " + pages;
+    }
+}
+
